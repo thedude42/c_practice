8e9b35ae2a1d789ca7a1f305313d51d2dd52c067
johnny <sk8rdie42@gmail.com>
2015-10-30 21:39:07 -0700

    Implemented c++ version of xpath query, adjusted makefile to it

--- xml/xpath_query.cpp
+++ xml/xpath_query.cpp
@@ -2,6 +2,10 @@
 #include <cstdlib>
 #include <string>
 #include <assert.h>
+#include <GetOpt.h>
+#include <vector>
+#include <dirent.h>
+#include <cctype>
 
 #include <libxml/tree.h>
 #include <libxml/parser.h>
@@ -10,43 +14,127 @@
 
 #if defined(LIBXML_XPATH_ENABLED) && defined(LIBXML_SAX1_ENABLED)
 
-const char SCHEMA_BASE[] = "schema/johnny/";
+using namespace std;
 
-int register_namespaces(xmlXPathContextPtr, const xmlChar*); 
+const string SCHEMA_BASE = "schema/johnny/";
+
 void print_xpath_nodes(xmlNodeSetPtr, FILE*); 
-xmlDocPtr getXmlDocFromFile(char* filename); 
+xmlDocPtr getXmlDoc(char* directory); 
+xmlDocPtr getXmlDocFromFile(string filename); 
 xmlXPathObjectPtr doXpathQuery(xmlDocPtr doc, const xmlChar* xpath);
 void get_primary_key(const char* classpath);
+string* usage();
+vector<string> getSchemas(string path);
 
 int
 main(int argc, char** argv) {
-    if((argc < 3) || (argc > 4)) {
-        cerr << "Error: wrong number of arguments." << endl;
+    if((argc < 2)) {
+        cerr << *usage() << endl;
         return(-1);
     } 
+    int primaryKey = 0;
+    char *queryStr = NULL;
+    char *directory = NULL;
+    int c, index;
     
+    opterr = 0;
+    while ((c = getopt (argc, argv, "pd:")) != -1)
+        switch (c) {
+            case 'p':
+                primaryKey = 1;
+                break;
+            case 'd':
+                directory = optarg;
+                break;
+            default:;
+        }
+    
+    index = optind;
+    if (index > argc) {
+        cerr << usage() << endl;
+        return -1;
+    }
+    queryStr = argv[index];  
     /* Init libxml */     
     LIBXML_TEST_VERSION
 
-    xmlDocPtr doc = getXmlDocFromFile(argv[1]);
+    xmlDocPtr doc = getXmlDoc(directory);
     if (!doc) {
-        cerr <<"main: problem getting xml from " << argv[1] << endl;
+        cerr << "main: problem getting xml from " << argv[1] << endl;
         return(-1);
     }
     
-    xmlXPathObjectPtr query_nodes = doXpathQuery(doc, BAD_CAST argv[2]);
-
-    if (!query_nodes) {
-        cerr << "xpath query :" << argv[1] <<  " on document " <<  argv[2] << " returned nothing\n" << endl;
-        return(1);
+    if (!primaryKey) {
+        xmlXPathObjectPtr query_nodes = doXpathQuery(doc, BAD_CAST queryStr);
+        
+        if (!query_nodes) {
+            cerr << "xpath query :" << argv[1] <<  " on document " <<  argv[2] << " returned nothing\n" << endl;
+            return(1);
+        }
+        print_xpath_nodes(query_nodes->nodesetval, stdout);
     }
-    print_xpath_nodes(query_nodes->nodesetval, stdout);
+    else {
+        get_primary_key(queryStr);
+    }
     return 0;
+}
+
+string*
+usage() {
+    string* str = new string("\
+usage:\n\
+    xpath_query [-p] query string\n\
+    -p: return primary key nodes associated with query string\n");
+    return str;
+}
+
+xmlDocPtr
+getXmlDoc(char* directory) {
+    vector<string> files;
+    vector<xmlDocPtr> schemaDocs;
+    xmlDocPtr next;
+    if (directory) {
+        files = getSchemas(string(directory));
+    }
+    else {
+        files = getSchemas(SCHEMA_BASE);
+    }
+    for(vector<string>::iterator it = files.begin(); it != files.end(); ++it) {
+        if (it->find(".xml") != string::npos)
+            if ((next = getXmlDocFromFile(SCHEMA_BASE+*it)))
+                schemaDocs.push_back(next); 
+    }
+    cout << "schemaDocs has " << schemaDocs.size() << " docs." << endl;
+    return getXmlDocFromFile(SCHEMA_BASE+string("ltm.xml"));
+}
+
+vector<string> getSchemas(string path) {
+    DIR*    dir;
+    dirent* pdir;
+    vector<string> files;
+    dir = opendir(path.c_str());
+
+    while ((pdir = readdir(dir))) {
+        files.push_back(pdir->d_name);
+    }
+    
+    return files;
 }
 
 void
 get_primary_key(const char* classpath) {
     
+}
+
+xmlDocPtr
+getXmlDocFromFile(string filename) {
+    xmlInitParser();
+    xmlDocPtr doc = xmlParseFile(filename.c_str());
+    if (doc == NULL) {
+        cerr << "Error: unable to parse file \"" << filename << "\"" << endl;
+        return(NULL);
+    }
+    return doc;
 }
 
 xmlXPathObjectPtr
@@ -57,14 +145,14 @@ doXpathQuery(xmlDocPtr doc, const xmlChar* xpath) {
 
     xpathCtx = xmlXPathNewContext(doc);
     if(xpathCtx == NULL) {
-        fprintf(stderr,"Error: unable to create new XPath context\n");
+        cerr << "Error: unable to create new XPath context" << endl;
         return NULL;
     }
 
     /* Evaluate xpath expression */
     xpathObj = xmlXPathEvalExpression(xpath, xpathCtx);
     if(xpathObj == NULL) {
-        fprintf(stderr,"Error: failed evaluating xpath expr => \"%s\"\n", xpath);
+        cerr << "Error: failed evaluating xpath expr => \""  << xpath << "\"" << endl;
         xmlXPathFreeContext(xpathCtx); 
         return(NULL);
     }
@@ -72,16 +160,6 @@ doXpathQuery(xmlDocPtr doc, const xmlChar* xpath) {
     return xpathObj;
 }
 
-xmlDocPtr
-getXmlDocFromFile(char* filename) {
-    xmlInitParser();
-    xmlDocPtr doc = xmlParseFile(filename);
-    if (doc == NULL) {
-        fprintf(stderr, "Error: unable to parse file \"%s\"\n", filename);
-        return(NULL);
-    }
-    return doc;
-}
 
 /**
  * print_xpath_nodes:
